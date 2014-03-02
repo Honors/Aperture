be96e74b63ee0fbdf09e05123fe16c9f342e21d1
Matt Neary <neary.matt@gmail.com>
2014-03-02 02:30:48 -0500

    custom controls using keyboard

--- Controls.js
+++ Controls.js
@@ -1,5 +1,21 @@
 var Controls = function(render, camera) {
-  controls = new THREE.OrbitControls( camera );
-  controls.addEventListener( 'change', render );
+  this.angle = 0;
+  this.camera = camera;
+  document.addEventListener('keydown', function(evt) {
+    if( evt.keyCode == 65 || evt.keyCode == 68 ) {
+      evt.preventDefault();
+      this.angle += (evt.keyCode == 65 ? 1 : -1) * Math.PI/50;
+      render();
+    }
+  }.bind(this));
+  this.render();
+};
+Controls.prototype.render = function() {
+  this.camera.up = new THREE.Vector3(0,0,1);
+  this.camera.lookAt(
+    new THREE.Vector3(Math.cos(this.angle) * 200, 
+      Math.sin(this.angle) * 200, 
+      0));
+  requestAnimationFrame(this.render.bind(this));
 };
 
