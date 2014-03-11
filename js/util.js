65461eb96b1350fcd68b8a4b88c40588b4831b62
Matt Neary <neary.matt@gmail.com>
2014-03-10 21:54:14 -0400

    js utility functions

---
+++ js/util.js
@@ -0,0 +1,24 @@
+var normalize = function(vec) {
+  return vec.map(divideBy(sqrt(vec.map(square).reduce(sum, 0))));
+};
+var divideBy = function(x) {
+  return function(y) {
+    return y/x;
+  };
+};
+var sqrt = Math.sqrt.bind(Math);
+var square = function(x) {
+  return x*x;
+};
+var sum = function(a, b) {
+  return a + b;
+};
+var match = function(maps, elm) {
+  return maps.reduce(function(fn, map) {
+    return fn || 
+      (elm instanceof map[0] ? 
+       map[1] : 
+       undefined);
+  }, undefined);
+};
+
