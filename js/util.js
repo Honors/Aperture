df9b0d893d7958c7aaa32ca3155023966be41163
Matt Neary <neary.matt@gmail.com>
2014-06-10 14:08:39 -0400

    better util functions for working with vectors and surfaces

--- js/util.js
+++ js/util.js
@@ -1,6 +1,7 @@
 var cos = Math.cos.bind(Math);
 var sin = Math.sin.bind(Math);
 var tan = Math.tan.bind(Math);
+var abs = Math.abs.bind(Math);
 var normalize = function(vec) {
   return vec.map(divideBy(sqrt(vec.map(square).reduce(sum, 0))));
 };
@@ -65,11 +66,84 @@ var piecewise = function(fns) {
   };
 };
 var revolvingParametric = function(f) {
-  return function(t, s) {
+  return function(h, theta) {
     return new THREE.Vector3(
-      cos(t) * f(s).x,
-      sin(t) * f(s).x,
-      f(s).y);
+      cos(theta) * f(h).x,
+      sin(theta) * f(h).x,
+      f(h).y);
   };
 };
-
+var drawLine = function(d, to) {
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(to);
+  var colors = [0xff0000, 0x00ff00, 0x0000ff];
+  var material = new THREE.LineBasicMaterial({ color: colors[d] });
+  return new THREE.Line(geometry, material);
+};
+var drawVector = function(pos, v) {
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(pos);
+  geometry.vertices.push(pos.clone().add(v));
+  var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+  return new THREE.Line(geometry, material);
+};
+var formZeroBasis = function(x, y, z) {
+  if( x && y && z ) {
+    throw new Error("Cannot form basis around null.");
+  } else if( x && y ) {
+    return [new THREE.Vector3(1, 0, 0),
+	    new THREE.Vector3(0, 1, 0),
+	    new THREE.Vector3(0, 0, 1)];
+  } else if( y && z ) {
+    return [new THREE.Vector3(0, 1, 0),
+	    new THREE.Vector3(0, 0, 1),
+	    new THREE.Vector3(1, 0, 0)];
+  } else if( x && z ) {
+    return [new THREE.Vector3(0, 0, 1),
+	    new THREE.Vector3(1, 0, 0),
+	    new THREE.Vector3(0, 1, 0)];
+  } else {
+    return false;
+  }
+};
+var formBasis = function(n) {
+  var b = null;
+  if( (b = formZeroBasis(n.x == 0, n.y == 0, n.z == 0)) ) {
+    return b;
+  }
+  var zprime = n.clone();
+  zprime.normalize();
+  var xprime;
+  if( zprime.z == 0 ) {
+    xprime = new THREE.Vector3(-(zprime.y + 2*zprime.z)/zprime.x, 1, 2);
+  } else {
+    xprime = new THREE.Vector3(1, 2, -(zprime.x + 2*zprime.y)/zprime.z);
+  }
+  xprime.normalize();
+  var yprime = new THREE.Vector3();
+  yprime.crossVectors(zprime, xprime);
+  yprime.normalize();
+  return [xprime, yprime, zprime];
+};
+var renderLines = function(ls, scene) {
+  ls.map(function(v) {
+    return v.clone().multiplyScalar(100);
+  }).map(function(v, d) {
+    return drawLine(d, v);
+  }).forEach(function(l) {
+    scene.add(l);
+  });
+};
+var surfaceBasisTransformer = function(basis) {
+  var xprime = basis[0];
+  var yprime = basis[1];
+  var zprime = basis[2];
+  return function(f) {
+    return function(h, theta) {
+      return xprime.clone().multiplyScalar(cos(theta) * f(h).x).add(
+	     yprime.clone().multiplyScalar(sin(theta) * f(h).x).add(
+	     zprime.clone().multiplyScalar(f(h).y)));
+    };
+  };
+};
